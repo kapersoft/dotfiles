019b8cca720075840ad6682a4e0739b5c697bd39
Jan Willem Kaper <kapersoft@gmail.com>
2025-08-09 15:18:41 +0200

    Add sync-obsidian script for automated Git operations in Obsidian directory

---
+++ bin/sync-obsidian.zsh
@@ -0,0 +1,27 @@
+pushd $HOME/Documents/Obsidian/Remindo
+
+# Pull the latest changes from the remote repository
+git pull --rebase
+
+# Check for changes in settings only
+DOT_CHANGES=$(git status --porcelain | grep -E '^(\?\?|\ M|\M |\A |\ D|\D ) \.')
+if [[ -n $DOT_CHANGES ]]; then
+    git add .[^.]*
+    git commit -m "$(date +%Y-%m-%d\ %H:%M:%S) Update settings"
+fi
+
+# Check for changes in notes only
+NOTES_CHANGES=$(git status --porcelain | grep -E '^(\?\?|\ M|\M |\A |\ D|\D ) ')
+if [[ -n $NOTES_CHANGES ]]; then
+    git add .
+    git commit -m "$(date +%Y-%m-%d\ %H:%M:%S) Update notes"
+fi
+
+# Check for unpushed commits
+UNPUSHED_COMMITS=$(git log --oneline @{u}..HEAD 2>/dev/null | wc -l)
+if [[ $UNPUSHED_COMMITS -gt 0 ]]; then
+    echo "Unpushed commits: $UNPUSHED_COMMITS"
+    git push
+fi
+
+popd
